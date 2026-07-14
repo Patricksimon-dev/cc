5eb84d60525524654f8a331a5492981614650617
Patricksimon-dev <simonogar025@gmail.com>
2026-07-14 19:54:54 +0000

    Use static homepage with API routes

--- api/index.js
+++ api/index.js
@@ -1,15 +1,5 @@
-const path = require('path');
-const fs = require('fs');
 const server = require('../server');
 
 const app = server.app;
-
-app.get('/', (_req, res) => {
-  const indexPath = path.join(__dirname, '..', 'index.html');
-  if (fs.existsSync(indexPath)) {
-    return res.sendFile(indexPath);
-  }
-  return res.status(404).send('index.html not found');
-});
 
 module.exports = app;
