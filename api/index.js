2297f1536616b64b290e45777e1de2dea33637af
Patricksimon-dev <simonogar025@gmail.com>
2026-07-14 19:51:55 +0000

    Make Vercel homepage entry explicit

--- api/index.js
+++ api/index.js
@@ -1,3 +1,15 @@
+const path = require('path');
+const fs = require('fs');
 const server = require('../server');
 
-module.exports = server.app;
+const app = server.app;
+
+app.get('/', (_req, res) => {
+  const indexPath = path.join(__dirname, '..', 'index.html');
+  if (fs.existsSync(indexPath)) {
+    return res.sendFile(indexPath);
+  }
+  return res.status(404).send('index.html not found');
+});
+
+module.exports = app;
